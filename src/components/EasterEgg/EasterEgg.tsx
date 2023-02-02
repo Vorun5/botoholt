77e7bcff2c0b68d9de3f4152dce77efdcb2d578a
Firdavsi <firdavsinurov326@gmail.com>
2023-02-02 18:56:16 +0300

    Edits

--- src/components/EasterEgg/EasterEgg.tsx
+++ src/components/EasterEgg/EasterEgg.tsx
@@ -2,8 +2,10 @@ import styles from "./EasterEgg.module.css";
 import {useState} from "react";
 import Emote from "./components/Emote";
 import Nickname from "./components/Nickname";
+import {useTranslation} from "react-i18next";
 
 const EasterEgg = () => {
+    const {t} = useTranslation();
     const [bigsOpen, setBigsOpen] = useState(false);
 
     return (
@@ -20,6 +22,20 @@ const EasterEgg = () => {
             </div>
             {bigsOpen ?
                 <div className={styles.bigs}>
+                    <span className={styles.bigs__title}>{t("special-thanks")}</span>
+
+                    <div className={styles.big}>
+                        <Emote path="/emotes/Meow.gif"/><Nickname nickname="Iron_Meow"/>
+                    </div>
+                    <div className={styles.big}>
+                        <Nickname nickname="TheAwkwardRabbit_ "/><Emote path="/emotes/big.png"/>
+                    </div>
+                    <div className={styles.big}>
+                        <Nickname nickname="kimonoska"/><Emote path="/emotes/kimonoska.png"/>
+                    </div>
+
+                    <span className={styles.bigs__title}>{t("bigs-and-pigs")}</span>
+
                     <div className={styles.big}>
                         !main<Nickname nickname="Enderoman"/><Emote path="/emotes/PoroGP.png"/>
                     </div>
@@ -48,16 +64,10 @@ const EasterEgg = () => {
                         <Nickname nickname="MrKoya"/>
                     </div>
                     <div className={styles.big}>
-                        <Nickname nickname="TheAwkwardRabbit_ "/><Emote path="/emotes/big.png"/>
+                        <Nickname nickname="LeamasRein_"/><Emote path="/emotes/PoroSex.png"/>
                     </div>
                     <div className={styles.big}>
-                        <Nickname nickname="kimonoska"/><Emote path="/emotes/kimonoska.png"/>
-                    </div>
-                    <div className={styles.big}>
-                        <Emote path="/emotes/Meow.gif"/><Nickname nickname="Iron_Meow"/>
-                    </div>
-                    <div className={styles.big}>
-                        <Emote path="/emotes/catChill.gif"/><Nickname nickname="easton"/>
+                        <Emote path="/emotes/catChill.gif"/><Nickname nickname="east__on"/>
                     </div>
                     <div className={styles.big}>
                         <Nickname nickname="Mobdyt"/><Emote path="/emotes/hug.png"/>
@@ -66,7 +76,13 @@ const EasterEgg = () => {
                         <Nickname nickname="hieteo"/><Emote path="/emotes/Gladge.png"/>
                     </div>
                     <div className={styles.big}>
+                        <Emote path="/emotes/BearBite.gif"/><Nickname nickname="lissa_lie"/>
+                    </div>
+                    <div className={styles.big}>
                         <Nickname nickname="Kreokid"/><Emote path="/emotes/Kreokid.gif"/>
+                    </div>
+                    <div className={styles.big}>
+                        <Nickname nickname="ZeroTwoZero_"/><Emote path="/emotes/Gayge.png"/>
                     </div>
                 </div> : <></>}
         </div>
