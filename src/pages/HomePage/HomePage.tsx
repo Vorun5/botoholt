81d1b4b9f46a6487d2d5daf8931ab470bfb56396
Firdavsi <firdavsinurov326@gmail.com>
2023-02-02 08:19:25 +0300

    Add about Botoholt

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -14,6 +14,14 @@ const HomePage = () => {
     return (
         <>
             <div className={styles.container}>
+                <div className={styles.about}>
+                    <h1 className={styles.about__title}>{t("about-botoholt")}</h1>
+                    <p className={styles.about__text}>
+                        <span className={styles.indent}/>{t("about-botoholt-text-1")}
+                        <br/> <span className={styles.indent}/> {t("about-botoholt-text-2")}
+                        <span className={styles.contact}>Urbinholt#0640</span>{t("about-botoholt-text-3")}
+                    </p>
+                </div>
                 {streamers.length === 0
                     ? <Loading/>
                     : <div className={styles.top}>
