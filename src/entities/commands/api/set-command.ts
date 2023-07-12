2e5c42186bffc79185ba3f85dd4d757346ba0b92
Firdavsi <firdavsinurov326@gmail.com>
2023-07-13 01:09:28 +0300

    real api

--- src/entities/commands/api/set-command.ts
+++ src/entities/commands/api/set-command.ts
@@ -2,10 +2,10 @@ import { api } from 'shared/api'
 import { AllPossibleCommandType } from 'shared/types'
 
 export const setCommand = async (command: AllPossibleCommandType): Promise<AllPossibleCommandType> => {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    if (Math.random() >= 0.5) throw new Error('Unlucky')
-    
-    return command
+    // await new Promise((resolve) => setTimeout(resolve, 2000))
+    // if (Math.random() >= 0.5) throw new Error('Unlucky')
+
+    // return command
 
     const response = await api.put('admin/commands', { json: command }).json<AllPossibleCommandType>()
     return response
